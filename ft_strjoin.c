95829cf11e1c7eea520bffb66df1a35342a22758
Jiheun Ko <jiko@c7r7s4.42seoul.kr>
2023-03-16 23:04:13 +0900

    jiko

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jiko <jiko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 21:54:32 by jiko              #+#    #+#             */
-/*   Updated: 2023/03/15 21:58:56 by jiko             ###   ########.fr       */
+/*   Updated: 2023/03/16 19:40:34 by jiko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,12 @@ char	*ft_strcat(char *dest, char *src)
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	char	*tmp;
 
+	tmp = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, 1);
+	if (!tmp)
+		return (tmp);
+	tmp = ft_strcat(tmp, (char *)s1);
+	tmp = ft_strcat(tmp, (char *)s2);
+	return (tmp);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jiko <jiko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:15:28 by jiko              #+#    #+#             */
-/*   Updated: 2023/03/15 19:14:40 by jiko             ###   ########.fr       */
+/*   Updated: 2023/03/16 22:47:26 by jiko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,8 @@ char			*ft_strnstr(const char *hay, const char *needle, size_t len);
 void			*ft_calloc(size_t count, size_t size);
 char			*ft_strdup(const char *s1);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strtrim(char const *s1, char const *set);
+char			**ft_split(char const *s, char c);
 
 #endif
