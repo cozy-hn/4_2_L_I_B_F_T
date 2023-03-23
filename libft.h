ffed26df87fe6cc902309d0d5b8c0642d8327e51
Jiheun Ko <jhk2721@gamil.com>
2023-03-23 22:24:11 +0900

    paoc

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jiko <jiko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:15:28 by jiko              #+#    #+#             */
-/*   Updated: 2023/03/20 20:14:57 by jiko             ###   ########.fr       */
+/*   Updated: 2023/03/23 22:20:41 by jiko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void		*ft_memset(void *str, int value, size_t size);
 size_t		ft_strlen(const char *s);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t		ft_strlcat(char *dest, char *src, size_t size);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
 int			ft_toupper(int c);
 int			ft_tolower(int c);
 char		*ft_strchr(const char *s, int c);
