6efe8bd3bbca5818fb9e2626a929d073ab0d86fc
Jiheun Ko <jhk2721@gmail.com>
2023-10-30 19:42:33 +0900

    sdf

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,29 +6,27 @@
 /*   By: jiko <jiko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 20:36:21 by jiko              #+#    #+#             */
-/*   Updated: 2023/03/23 22:27:42 by jiko             ###   ########.fr       */
+/*   Updated: 2023/10/30 19:31:44 by jiko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *str)
 {
-	long long	numminus;
 	long long	num;
 
-	numminus = 1;
 	num = 0;
 	while ((*str >= 9 && *str <= 13) || *str == 32)
 		str++;
-	if (*str == '+' || *str == '-')
-	{
-		if (*str == '-')
-			numminus *= -1;
+	if (*str == '-')
+		return (-1);
+	if (*str == '+')
 		str++;
-	}
 	while (*str >= '0' && *str <= '9')
 	{
 		num = num * 10 + (*str - '0');
 		str++;
+		if (num >= 99999)
+			return (-1);
 	}
-	return ((int)(numminus * num));
+	return ((int)(num));
 }
